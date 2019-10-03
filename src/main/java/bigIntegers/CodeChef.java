49deff41050de92ffb1ebc843630130daa77c0f4
Gibran <gibran.khan@imaginea.com>
2019-10-03 23:44:35 +0530

    BigInteger added.

---
+++ src/main/java/bigIntegers/CodeChef.java
@@ -0,0 +1,24 @@
+package bigIntegers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class CodeChef {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int n = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(factorial(n));
+    }
+
+    private static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
+
